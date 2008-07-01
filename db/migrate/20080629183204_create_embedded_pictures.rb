fa82dd14028a908864546cf835ee55daf5e0fa28
Michel Belleville <michel.belleville@gmail.com>
2008-07-01 20:03:10 +0000

    Debugged a migration issue on embedded_pictures.

--- db/migrate/20080629183204_create_embedded_pictures.rb
+++ db/migrate/20080629183204_create_embedded_pictures.rb
@@ -2,7 +2,7 @@ class CreateEmbeddedPictures < ActiveRecord::Migration
   def self.up
     create_table :embedded_pictures do |t|
       t.integer :page_id
-      t.integer :page_id
+      t.integer :picture_id
       t.integer :rank
 
       t.timestamps
