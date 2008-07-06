3d1014a6c8f91dfd72b3b158178495ff0c80b6e6
Michel Belleville <michel.belleville@gmail.com>
2008-07-06 10:34:03 +0000

    Re-made properly thumbnail recropping.

---
+++ app/controllers/thumbnails_controller.rb
@@ -0,0 +1,25 @@
+class ThumbnailsController < ApplicationController
+  # GET /pictures/1/thumbnails/new
+  def new
+    @picture = Picture.find params[:picture_id]
+    
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+  
+  # POST /pictures/1/thumbnails
+  def create
+    @picture = Picture.find params[:picture_id]
+    
+    respond_to do |format|
+      if @picture.recrop_thumb(params[:left], params[:top], params[:version].to_sym)
+        flash[:notice] = 'Thumbnail was successfully cropped.'
+        format.html { redirect_to(@picture) }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -19,4 +19,32 @@ class Picture < ActiveRecord::Base
                                                        'image/gif' ]
   validates_attachment_thumbnails :attachment
   
+  # access to the image's dimensions
+  def dimensions( type = :original )
+    command = <<-end_command
+      #{ Paperclip.path_for_command('identify') }
+      -format "{:width => %[fx:w], :height => %[fx:h]}"
+      "#{ attachment.path type }"
+    end_command
+    eval(%x[#{command.gsub(/\s+/, " ")}])
+  end
+  
+  # thumbnail recropping
+  def recrop_thumb(left, top, version = :original, factor = 1)
+    dims = self.dimensions(:thumb)
+    command = <<-end_command
+      #{ Paperclip.path_for_command('convert') }
+      "#{ File.expand_path(self.attachment.path(version)) }"
+      -crop #{dims[:width] * factor}x#{dims[:height] * factor}+#{left}+#{top} +repage
+      -resize #{dims[:width]}x#{dims[:height]}
+      "#{ File.expand_path(self.attachment.path(:thumb)) }"
+    end_command
+    success = system(command.gsub(/\s+/, " "))
+    
+    if success && $?.exitstatus != 0 && @whiny_thumbnails
+      raise PaperclipError, "There was an error processing this thumbnail"
+    end
+    
+    success
+  end
 end
