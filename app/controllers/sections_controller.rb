dff7c823a467fe58803391f0078bf0a013f53576
Michel Belleville <michel.belleville@gmail.com>
2008-08-06 06:41:41 +0000

    Remodelled the section and page views.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -16,7 +16,8 @@ class SectionsController < ApplicationController
   # GET /sections/1.xml
   def show
     @section = Section.find(params[:id])
-
+    @page = @section.pages.first
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @section }
